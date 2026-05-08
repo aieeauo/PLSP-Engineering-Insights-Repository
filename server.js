58282dbf2100b75049960cc2623dd781ef1f522d
Anniejel Llaguno <anniejeldejesusllaguno@gmail.com>
2026-05-09 06:29:11 +0800

    Did some changes

--- server.js
+++ server.js
@@ -333,7 +333,8 @@ app.get('/api/resources/latest', async (req, res) => {
     }
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Server is running on port ${PORT}`);
 });
